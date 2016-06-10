d02a7fa4500261828ce71b8b3200692257554b62
soohyunkim <soohyunkim@movingimage.com>
2016-06-10 12:51:12 +0200

    patrick modified

--- classifier.php
+++ classifier.php
@@ -40,7 +40,7 @@ class FaceDetector{
             'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
             'ffprobe.binaries' => '/usr/bin/ffprobe',
             'timeout'          => 3600, // The timeout for the underlying process
-            'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+            'ffmpeg.threads'   => 16,   // The number of threads that FFMpeg should use
         ));
 
         $video = $ffmpeg->open($videoFile);
